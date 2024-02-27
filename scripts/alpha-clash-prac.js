2650ed20ea8ab912d01d128803b92fdb745a637b
ABU SAID <abusaid@ABUs-MacBook-Air.local>
2024-02-27 18:47:51 +0600

    complete alpha clash

--- scripts/alpha-clash-prac.js
+++
@@ -1,9 +0,0 @@
-function play(){
-    // hide home-screen section
-    const homeScreen = document.getElementById('home-screen');
-    homeScreen.classList.add('hidden');
-    
-    // show playground Section 
-    const playground = document.getElementById('playground-section');
-    playground.classList.remove('hidden');
-}
